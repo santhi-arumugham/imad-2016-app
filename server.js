0a9075450b1fb48e6deaada1f6999b1764900d63
santhi-arumugham <santhi.s.arumugham@gmail.com>
2016-10-11 19:35:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+aap.get('/article1', function (req, res) {
+    res.send('Article 1 & will be here'));
+});
+
+aap.get('/article2', function (req, res) {
+    res.send('Article 2 & will end here'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
